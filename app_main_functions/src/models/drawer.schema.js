a98f4356b3bebcf538ba1c3a11fe9d2a6d95513a
sandeep chakaldar <sandeep.chakaldar@jai-kisan.com>
2020-10-26 21:41:14 +0530

    drawer fix

--- app_main_functions/src/models/drawer.schema.js
+++ app_main_functions/src/models/drawer.schema.js
@@ -2,32 +2,34 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const drawerSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
   merchantId: {
     type: String,
     required: true,
   },
-  sub_section: {
-    type: Boolean,
-    required: true,
-  },
-  sub: {
-    type: Array,
-    name:
+  list_drawer: [
     {
-      type: String
-    },
-  },
+      name: {
+        type: String
+      },
+      sub_section: {
+        type: Boolean
+      },
+      sub: [
+        {
+          name: {
+            type: String
+          }
+        }
+      ]
+    }
+  ],
   is_active: {
     type: Boolean,
     required: true,
     default: true,
   },
-  created_at: Date,
-  updated_at: Date,
+  created_at: Number,
+  updated_at: Number,
 });
 
 const DrawerWidget = mongoose.model("DrawerWidget", drawerSchema);
